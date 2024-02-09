13baceedee775a150e290f8842756f348a49a57a
Mariana Bondarenko <mbondarenko@llnw.com>
2024-02-09 13:04:50 +0100

    Fix test_access_rule_advanced_settings_input_fields test

--- ltf2/console_app/tests/security/test_security_access_rules.py
+++ ltf2/console_app/tests/security/test_security_access_rules.py
@@ -176,7 +176,6 @@ def test_access_rule_advanced_settings_input_fields(access_rules_page: SecurityP
     access_rules_page.extension_blacklist.fill(string_value1)
     # Header Blacklist
     access_rules_page.header_blacklist.fill(string_value1)
-
     # Save rule
     access_rules_page.save.click()
     assert access_rules_page.client_snackbar.text_content() == "Access rule created"
@@ -197,10 +196,11 @@ def test_access_rule_advanced_settings_input_fields(access_rules_page: SecurityP
     assert access_rules_page.file_upload_limit.input_value() == upload_limit, \
         "Wrong Single File Upload Limit"
     # Allowed Request Content Types
-    assert access_rules_page.request_content_type_buttons[0].text_content() == string_value1, \
+    assert access_rules_page.request_content_type_buttons[-1].text_content() == string_value1, \
         f"Cannot find `{string_value1}` in Allowed Request Content Types"
     # Extension Blacklist
-    assert access_rules_page.extension_blacklist_buttons[0].text_content() == string_value1, \
+    access_rules_page.extension_blacklist_buttons[-1].click()
+    assert access_rules_page.extension_blacklist_buttons[-1].text_content() == string_value1, \
         f"Cannot find `{string_value1}` in Extension Blacklist"
     # Header Blacklist
     assert access_rules_page.header_blacklist_buttons[0].text_content() == string_value1, \
